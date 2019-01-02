ffc053982d7f79e7092069a71c2b640aed180c78
Y. Z. Chen <754097987@qq.com>
2019-01-02 12:40:28 +0800

    [Rebuild] skeleton.

--- build.sh
+++ build.sh
@@ -1,10 +1,46 @@
 #!/bin/bash
 
-buildpdf() {
+build_count() {
+    bc -q <<< $(cat build.txt)+1 > build.txt
+}
+
+makepdf() {
     pdflatex --shell-escape -synctex=1 -interaction=nonstopmode -output-directory=build "$1".tex
 }
 
-bc -q <<< $(cat build.txt)+1 > build.txt
+makemd() {
+	echo
+}
 
-buildpdf cover
-buildpdf book
+makepreamble() {
+	echo "\documentclass[12pt, a4paper]{book}" >  build/book.tex
+	echo                                       >> build/book.tex
+	echo "\usepackage{pdfpages}"               >> build/book.tex
+	echo                                       >> build/book.tex
+	echo "\begin{document}"                    >> build/book.tex
+}
+
+makefinal() {
+	echo "\end{document}"                      >> build/book.tex
+	makepdf build/book
+}
+
+includepdf() {
+    makepdf "$1"
+    echo "\includepdf{build/$1.pdf}"           >> build/book.tex
+}
+
+includemd() {
+	echo
+}
+
+build_count
+
+# Preamble
+makepreamble
+
+# Write book
+includepdf cover
+
+# End
+makefinal
