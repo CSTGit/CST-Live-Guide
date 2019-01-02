643beb99b1d2e96c59b12728c847e55077619f0d
Y. Z. Chen <754097987@qq.com>
2019-01-03 03:58:05 +0800

    [Update] skeleton of book. However, many things needs to do.

--- build.sh
+++ build.sh
@@ -16,15 +16,16 @@ makemd() {
 
 makepreamble() {
     mkdir build
-    echo "\documentclass[12pt, a4paper]{book}" >  build/book.tex
-    echo                                       >> build/book.tex
-    echo "\usepackage{pdfpages}"               >> build/book.tex
-    echo                                       >> build/book.tex
-    echo "\begin{document}"                    >> build/book.tex
+    echo "\documentclass[12pt, a4paper]{book}"  >  build/book.tex
+    echo                                        >> build/book.tex
+    echo "\usepackage{pdfpages}"                >> build/book.tex
+    echo                                        >> build/book.tex
+    echo "\begin{document}"                     >> build/book.tex
+    echo "\pagenumbering{gobble}"               >> build/book.tex
 }
 
 makefinal() {
-    echo "\end{document}"                      >> build/book.tex
+    echo "\end{document}"                       >> build/book.tex
     makepdf /build book
     retval=$?
     mkdir output
@@ -34,13 +35,13 @@ makefinal() {
 
 includepdf() {
     # TODO: make TableOfContents
-    echo "\includepdf{build/$1/$2.pdf}"           >> build/book.tex
+    echo "\includepdf[pagecommand={\thispagestyle{plain}}]{build/$1/$2.pdf}"         >> build/book.tex
     makepdf /"${1%/}" "$2"
 }
 
 includemd() {
     # TODO: make TableOfContents
-    echo "\includepdf{build/$1/$2.pdf}"           >> build/book.tex
+    echo "\includepdf[pagecommand={\thispagestyle{plain}}]{build/$1/$2.pdf}"         >> build/book.tex
     makemd /"${1%/}" "$2"
 }
 
@@ -48,12 +49,17 @@ build_count
 
 # Preamble
 rm -r build
+rm -r output
 makepreamble
 
 # Write book
 # for example: 
 # includepdf 1-NumberBasics b
 includepdf / cover
+echo "\tableofcontents"                         >> build/book.tex
+echo "\newpage"                                 >> build/book.tex
+echo "\pagenumbering{arabic}"                   >> build/book.tex
 
 # End
 makefinal
+xdg-open output/book.pdf
