0fc35d8629c32362700539d6a95151089c9c630d
Y. Z. Chen <754097987@qq.com>
2019-01-03 17:03:00 +0800

    [Update] skeleton.

--- build2.sh
+++ build2.sh
@@ -10,17 +10,17 @@ makepdf() {
 }
 
 makemd() {
-    mkdir build$1
+	mkdir build$1
     pandoc -t latex -o build${1%/}/"$2".pdf ."$1"/"$2".md
 }
 
 makepreamble() {
-    mkdir build
+	mkdir build
     echo "\documentclass[12pt, a4paper]{book}"  >  build/book.tex
     echo                                        >> build/book.tex
     echo "\usepackage{pdfpages}"                >> build/book.tex
     echo "\usepackage{import}"                  >> build/book.tex
-    echo "\usepackage[subpreambles=true]{standalone}"   >> build/book.tex
+    echo "\usepackage{subfiles}"   				>> build/book.tex
     echo                                        >> build/book.tex
     echo "\begin{document}"                     >> build/book.tex
     echo "\pagenumbering{gobble}"               >> build/book.tex
@@ -37,16 +37,15 @@ makefinal() {
 }
 
 includetex() {
-    # TODO: make TableOfContents
-    echo "\subfile{$1$2}"         >> build/book.tex
-    # echo "\import{$1}{$2}" >> build/book.tex
-    makepdf /"${1%/}" "$2"
+	if [ $1 = / ]; then
+		echo "\subfile{$2}" >> build/book.tex
+	else
+		echo "\subfile{${1%/}/$2}" >> build/book.tex
+	fi
 }
 
 includemd() {
-    # TODO: make TableOfContents
-    echo "\includepdf[pagecommand={\thispagestyle{plain}\section{title}]}, offset=75 -75]{build/$1/$2.pdf}"         >> build/book.tex
-    makemd /"${1%/}" "$2"
+	
 }
 
 build_count
@@ -63,9 +62,9 @@ includetex / cover
 echo "\tableofcontents"                         >> build/book.tex
 echo "\newpage"                                 >> build/book.tex
 echo "\pagenumbering{arabic}"                   >> build/book.tex
-includetex Cpt1/ Sample1
-includetex Cpt2/ Sample1
-includetex Cpt3/ Sample1
+includetex Cpt1 Sample1
+includetex Cpt2 Sample1
+includetex Cpt3 Sample1
 
 # End
 makefinal
